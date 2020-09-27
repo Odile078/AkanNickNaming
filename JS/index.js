7fdf7cca588287519a13d4322ab994e8d391e910
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-09-27 21:29:46 +0200

    adding validation for month input

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,40 @@
 /*Validating the date input  */
 function datevalidate(){
-var date=document.gform.day.value;
-if( date==""||isNAN(date) ||  date.length<2 ){
+    var date=document.gform.day.value;
+    if( date==""||isNAN(date) ||  date.length<2 ){
         
-    if( date < 1 || date > 31){
-       document.getElementById("inputd").innerHTML="Please enter the date in numbers(1-31)";
-       return false;
+        if( date < 1 || date > 31){
+           document.getElementById("inputd").innerHTML="Please enter the date in numbers(1-31)";
+           return false;
+       }
    }
+   else if(date.length>2){
+       document.getElementById("inputd").innerHTML="Please enter a valid date";
+           return false;
+   }
+   else{
+       return true;
+   }
+   
+  
 }
-else if(date.length>2){
-   document.getElementById("inputd").innerHTML="Please enter a valid date";
-       return false;
-}
-else{
-   return true;
-}
-
-
+/* Validation for month input*/
+function monthvalidate(){
+   var months=document.gform.month.value;
+   if( months==""||isNAN(months) ||  months.length<2 ){
+       
+        if( months < 1 || months > 12){
+          document.getElementById("inputm").innerHTML="Please enter the date in numbers(1-31)";
+          return false;
+        }
+    }
+    else if(months.length>2){
+      document.getElementById("inputm").innerHTML="Please enter a valid date";
+          return false;
+    }
+    else{
+      return true;
+    }
+  
+   
 }
